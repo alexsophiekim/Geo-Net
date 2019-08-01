230ef64ffcf3515aeefa70b0f38f092ebd2243ea
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2019-08-01 22:47:21 +0000

    trying to make ajax and setInterval

--- js/script.js
+++ js/script.js
@@ -1,26 +1,43 @@
 $(document).ready(function(){
   // console.log('ready');
 
-  $("#measureBtn").click(){
 
-  };
 
-  $("#reportBtn").click(){
+    $("#measureBtn").click(function(){
 
-  };
+      $("#result").append();
+      setLoadInterval();
 
-  $.ajax({
-    type: 'GET',
-    url: 'https://api.geonet.org.nz/intensity?type=measured',
-    dataType: 'json',
-    success: function(dataFromJSON){
-      console.log(dataFromJSON);
-    },
-    erroe: function(){
-      console.log('something has gone wrong');
+
+    });
+
+    $("#reportBtn").click(function(){
+
+
+    });
+
+
+
+    function callData(){
+      $.ajax({
+        type: 'GET',
+        url: 'https://api.geonet.org.nz/intensity?type=measured',
+        dataType: 'json',
+        success: function(dataFromJSON){
+          console.log(dataFromJSON);
+
+        },
+        erroe: function(){
+          console.log('something has gone wrong');
+        }
+      });  //ajax end
     }
-  })
 
 
+    setInterval(function(){
+          console.log('tick');
+          callData('report',reportCol);
+          callData('measure',measureCol);
+        }, 1000);
 
 });
