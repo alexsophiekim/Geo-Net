08696fdb873367b45aa30415f5d67dc12b98f63e
Sophie Kim <270010565@bun2-206-17.local>
2019-08-09 11:19:09 +1200

    task done

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Geo-Net
+
+[GeoNet API](https://api.geonet.org.nz/)
+
+## Installation
+To install everything needed for this project you need to have a version of Node JS and NPM installed on your machine.
+
+```sh
+$ git clone https://github.com/19WDWU02/Geo-Net.git
+$ cd Geo-Net
+$ npm install
+```
+
+## Task
+You are to use the GeoNet api to create an application which showcases any type of data which you can get from their API.  
+Look through all the different endpoints to see what data you can retrieve.  
+You don't need to create a 'pretty' front-end interface for it at this stage, but rather just get the data and then view it on the screen in an appropriate way.  
+ie. If you only get Lat & Lng values, then an appropriate way would be to view it on a map rather than just a list of coordinates
--- js/script.js
+++ js/script.js
@@ -1,43 +1,58 @@
 $(document).ready(function(){
-  // console.log('ready');
+  const reportedContainer = $('#reportedContainer');
+  const measuredContainer = $('#measuredContainer');
 
 
-
-    $("#measureBtn").click(function(){
-
-      $("#result").append();
-      setLoadInterval();
-
-
-    });
-
-    $("#reportBtn").click(function(){
-
-
-    });
-
-
-
-    function callData(){
-      $.ajax({
-        type: 'GET',
-        url: 'https://api.geonet.org.nz/intensity?type=measured',
-        dataType: 'json',
-        success: function(dataFromJSON){
-          console.log(dataFromJSON);
-
-        },
-        erroe: function(){
-          console.log('something has gone wrong');
+  $("#getReported").click(function(){
+    $.ajax({
+      type: 'GET',
+      url: 'https://api.geonet.org.nz/intensity?type=reported',
+      dataType: 'json',
+      success: function(dataFromJSON){
+        reportedContainer.empty();
+        if (dataFromJSON.features.length === 0) {
+          reportedContainer.append(`There hasn't been any reported earthquakes`);
+        } else {
+          dataFromJSON.features.map(function(quake){
+            reportedContainer.append(`<li>${quake.geometry.coordinates[0]}, ${quake.geometry.coordinates[1]} - MMI #${quake.properties.mmi}</li>`);
+          }) //get lat, long - append where and how big
         }
-      });  //ajax end
-    }
+      },
+      error: function(){
+        console.log('something has gone wrong');
+      }
+    })
+  })
+
+  $("#getMeasured").click(function(){
+    $.ajax({
+      type: 'GET',
+      url: 'https://api.geonet.org.nz/intensity?type=measured',
+      dataType: 'json',
+      success: function(dataFromJSON){
+        measuredContainer.empty();
+        if (dataFromJSON.features.length === 0) {
+          measuredContainer.append(`There hasn't been any measured earthquakes`);
+        } else {
+          dataFromJSON.features.map(function(quake){
+            measuredContainer.append(`<li>${quake.geometry.coordinates[0]}, ${quake.geometry.coordinates[1]} - MMI #${quake.properties.mmi}</li>`);
+          })
+        }
+
+      },
+      error: function(){
+        console.log('something has gone wrong');
+      }
+    });  //ajax end
+  })
+
+
 
 
     setInterval(function(){
           console.log('tick');
-          callData('report',reportCol);
-          callData('measure',measureCol);
-        }, 1000);
+          getQuakeData('reported',reportedContainer);
+          getQuakeData('measured',measuredContainer);
+        }, 5000);
 
-});
+});   // js end
